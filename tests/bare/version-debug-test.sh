f22fe382adc96d0fa21e0ace63328ca9c18ef390
francesco <info.francescobianco@gmail.com>
2022-11-15 18:30:40 +0100

    First commit

--- tests/bare/version-debug-test.sh
+++ tests/bare/version-debug-test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 bash target/debug/mush
-bash target/debug/mush --help
-bash target/debug/mush build --help
+
+bash target/debug/mush --version
+
+bash target/debug/mush --version build
