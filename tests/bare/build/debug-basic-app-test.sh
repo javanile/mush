777a8aaa1a3b65e8895d3372759ad1fe14c5f79f
francesco <info.francescobianco@gmail.com>
2022-11-18 12:43:42 +0100

    First commit

--- tests/bare/build/debug-basic-app-test.sh
+++ tests/bare/build/debug-basic-app-test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
+## Build Mush
 cp target/debug/mush target/debug/mush.sh
 bash target/debug/mush.sh build --target dist
 
+## Build Basic App
 cd tests/fixtures/basic-app
-
 bash ../../../target/dist/mush.sh --vv build --target debug
