b9dc9cfa7885253774c701f135d3c899b2d796d9
francescobianco <info.francescobianco@gmail.com>
2023-09-07 01:35:29 +0200

    First commit

--- target/dist/mush.sh
+++ target/dist/mush.sh
@@ -408,7 +408,7 @@ embed_file() {
   local module_name=$1
   local module_file=$2
 
-  echo "$module_name() {"
+  echo "${module_name}() {"
   echo "  cat <<'EOF'"
   #cat "$module_file" | tr -s '\n'
   sed '/^[[:space:]]*$/d' "${module_file}"
