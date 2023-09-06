c2cdce482f06b5362c9118de39e5f30e204edbea
francesco <info.francescobianco@gmail.com>
2023-09-06 13:16:33 +0200

    First commit

--- Makefile
+++ Makefile
@@ -92,3 +92,6 @@ test-package-manager-git:
 test-basic-app:
 	@bash tests/bare/basic-app-test.sh
 
+test-build-rust-app:
+	@bash tests/bare/build/rust-app-test.sh
+
--- tests/bare/basic-app-test.sh
+++ tests/bare/basic-app-test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Prepare system for testing..."
+echo "------------------------------------------------------------------------------"
+
 mush build -v --target dist
 sudo make install
 
+echo "Start testing basic-app..."
+echo "------------------------------------------------------------------------------"
 
+mush build
 
