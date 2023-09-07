87941be5fd2412b1135a25db58634066a1a07982
francescobianco <info.francescobianco@gmail.com>
2023-09-08 01:58:36 +0200

    Nightly Release

--- src/main.sh
+++ src/main.sh
@@ -11,7 +11,7 @@ module tasks
 
 legacy getoptions
 
-VERSION="Mush 0.1.0 (2023-09-07)"
+VERSION="Mush 0.1.1 (2023-09-07)"
 
 parser_definition() {
   setup REST error:args_error help:usage abbr:true -- "Shell's build system" ''
