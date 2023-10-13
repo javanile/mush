62f6064c0110e289e50a19225ea3a16ff4142cfa
francesco <info.francescobianco@gmail.com>
2023-10-13 14:34:57 +0200

    Minor changes

--- src/main.sh
+++ src/main.sh
@@ -11,7 +11,7 @@ module tasks
 
 legacy getoptions
 
-VERSION="Mush 0.1.1 (2023-09-19)"
+VERSION="Mush 0.1.1 (2023-10-13)"
 
 parser_definition() {
   setup REST error:args_error help:usage abbr:true -- "Shell's build system" ''
