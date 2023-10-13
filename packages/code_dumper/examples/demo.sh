e481bec948296eea4ae94653e6dc8e0f5918dd29
francesco <info.francescobianco@gmail.com>
2023-10-13 19:01:23 +0200

    First commit

--- packages/code_dumper/examples/demo.sh
+++ packages/code_dumper/examples/demo.sh
@@ -1,4 +1,44 @@
 
+trap 'ERRO_LINENO=$LINENO' ERR
+trap '_failure' EXIT
+
+_failure() {
+  ERR_CODE=$? # capture last command exit code
+  set +xv # turns off debug logging, just in case
+  if [[  $- =~ e && ${ERR_CODE} != 0 ]]
+  then
+    echo -e "\b\b"
+      # only log stack trace if requested (set -e)
+      # and last command failed
+      echo "!========= CATASTROPHIC COMMAND FAIL ========="
+      echo
+      echo "SCRIPT EXITED ON ERROR CODE: ${ERR_CODE}"
+      echo
+      LEN=${#BASH_LINENO[@]}
+      for (( INDEX=0; INDEX<$LEN-1; INDEX++ ))
+      do
+          echo '---'
+          echo "FILE: $(basename ${BASH_SOURCE[${INDEX}+1]})"
+          echo "  FUNCTION: ${FUNCNAME[${INDEX}+1]}"
+          if [[ ${INDEX} > 0 ]]
+          then
+           # commands in stack trace
+              echo "  COMMAND: ${FUNCNAME[${INDEX}]}"
+              echo "  LINE: ${BASH_LINENO[${INDEX}]}"
+          else
+              # command that failed
+              echo "  COMMAND: ${BASH_COMMAND}"
+              echo "  LINE: ${ERRO_LINENO}"
+          fi
+      done
+      echo
+      echo "======= END CATASTROPHIC COMMAND FAIL ======="
+      echo
+  fi
+}
+
+
 main() {
+  cavallo
   code_dumper_error "examples/demo.sh" "3"
 }
--- packages/code_dumper/src/lib.sh
+++ packages/code_dumper/src/lib.sh
@@ -1,4 +1,6 @@
 
+cane
+
 code_dumper_error() {
   echo -e "\e[K\e[0m\e[1m\e[38;5;9merror\e[0m\e[0m\e[1m: expected item, found keyword \`if\`\e[0m"
   echo -e "\e[0m  \e[0m\e[0m\e[1m\e[38;5;12m--> \e[0m\e[0msrc/main.rs:39:1\e[0m"
