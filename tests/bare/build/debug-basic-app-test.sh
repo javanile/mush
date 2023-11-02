89862ceb0a51043fd7b104fd2e72034de11ec8fa
francescobianco <info.francescobianco@gmail.com>
2023-11-02 01:11:43 +0100

    First commit

--- tests/bare/build/debug-basic-app-test.sh
+++ tests/bare/build/debug-basic-app-test.sh
@@ -3,9 +3,9 @@ set -e
 
 ## Build Mush
 cp target/debug/mush target/debug/mush.sh
-bash target/debug/mush.sh build --target debug
-bash target/debug/mush build --target dist
+bash target/debug/mush.sh build
 
 ## Build Basic App
 cd tests/fixtures/basic-app
-bash ../../../target/dist/mush.sh --vvv build --target debug
+rm -fr target lib && true
+bash ../../../target/debug/mush -vvv build
