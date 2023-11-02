9e491ea7c0cde545d9fac37430a5a31c594e2aa2
francesco <info.francescobianco@gmail.com>
2023-11-02 17:56:06 +0100

    First commit

---
+++ tests/fixtures/basic-app/target/debug/packages/error_trace/lib.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+## BP010: Release metadata
+## @build_type: lib
+## @build_date: 2023-11-02T16:54:19Z
+set -e
+extern() {
+  extern=$1
+}
+legacy() {
+  legacy=$1
+}
+module() {
+  module=$1
+}
+public() {
+  public=$1
+}
+use() {
+  use=$1
+}
+embed() {
+  embed=$1
+}
+## BP004: Compile the entrypoint
+
+__feature_error_dumper__hook_build_debug_head_section() {
+  local value
+
+  value=$1
+
+  if [ "${value}" = "true" ]; then
+    echo "Adding error dumper to the build"
+  fi
+}
