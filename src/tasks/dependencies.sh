30133c7aaaaf5f767550fe0608e9509a9b57ffb5
francesco <info.francescobianco@gmail.com>
2023-11-02 15:37:30 +0100

    First commit

--- src/tasks/dependencies.sh
+++ src/tasks/dependencies.sh
@@ -1,43 +1,56 @@
 
 exec_dependencies() {
-  process_dependencies
-  process_dependencies_build
-  process_dev_dependencies
-  process_dev_dependencies_build
+  process_dependencies dev
+  process_dependencies_build dev
+
+  process_dependencies prod
+  process_dependencies_build prod
 }
 
 process_dependencies() {
-  echo "${MUSH_DEPS}" | while IFS=$'\n' read -r dependency && [ -n "$dependency" ]; do
+  local dependencies_type
+  local dependencies_list
+
+  dependencies_type=$1
+  if [ "${dependencies_type}" = "prod" ]; then
+    dependencies_list="${MUSH_DEPS}"
+  else
+    dependencies_list="${MUSH_DEV_DEPS}"
+  fi
+
+  echo "${dependencies_list}" | while IFS=$'\n' read -r dependency && [ -n "$dependency" ]; do
     [ "${VERBOSE}" -gt 4 ] && echo "Parsing dependency '$dependency'"
 
     local package_name=${dependency%=*}
     local package_signature=${dependency#*=}
 
     if [ ! -d "${MUSH_DEPS_DIR}/${package_name}" ]; then
-      process_dependency "$package_name" "$package_signature"
+      process_dependency "${dependencies_type}" "${package_name}" "${package_signature}"
     fi
   done
 }
 
 process_dependency() {
+  local dependencies_type
   local package_name
   local package_source
   local package_full_name
   local package_version_constraint
 
-  package_name="$1"
+  dependencies_type="$1"
+  package_name="$2"
 
-  if [ "$2" = "*" ]; then
+  if [ "$3" = "*" ]; then
     package_source="mush"
     package_full_name="${package_name}"
     package_version_constraint="*"
   else
-    package_source="${2%% *}"
+    package_source="${3%% *}"
     package_full_name=$(echo "$your_variable" | awk '{print $2}')
     package_version_constraint=$(echo "$your_variable" | awk '{print $3}')
   fi
 
-  [ "${VERBOSE}" -gt 4 ] && echo "Processing dependency '$1', '$2', 'source=${package_source}'"
+  [ "${VERBOSE}" -gt 4 ] && echo "Processing '$1' dependency '$2', '$3', 'source=${package_source}'"
 
   case "${package_source}" in
     git)
@@ -54,48 +67,17 @@ process_dependency() {
 }
 
 process_dependencies_build() {
-  echo "${MUSH_DEPS_BUILD}" | while IFS=$'\n' read dependency && [ -n "$dependency" ]; do
-    local package_name=${dependency%=*}
-    local package_script=${dependency#*=}
-    local package_dir="${MUSH_DEPS_DIR}/${package_name}"
+  local dependencies_type
+  local dependencies_build
 
-    if [ -d "${package_dir}" ]; then
-      local pwd=$PWD
-      cd "${package_dir}"
-      eval "PATH=${PATH}:${PWD} ${package_script}"
-      cd "$pwd"
-    fi
-  done
-}
+  dependencies_type=$1
+  if [ "${dependencies_type}" = "prod" ]; then
+    dependencies_build="${MUSH_DEPS_BUILD}"
+  else
+    dependencies_build="${MUSH_DEV_DEPS_BUILD}"
+  fi
 
-process_dev_dependencies() {
-  echo "${MUSH_DEV_DEPS}" | while IFS=$'\n' read -r dependency && [ -n "$dependency" ]; do
-    local package_name=${dependency%=*}
-    local package_signature=${dependency#*=}
-
-    if [ ! -d "${MUSH_DEPS_DIR}/${package_name}" ]; then
-      process_dev_dependency "$package_name" "$package_signature"
-    fi
-  done
-}
-
-process_dev_dependency() {
-  case "$2" in
-    git)
-      git_dependency "$1" "$3" "$4"
-      ;;
-    mush)
-      mush_dependency "$1" "$3" "$4"
-      ;;
-    *)
-      console_error "Unsupported package manager '$2' for '$1' on Manifest.toml"
-      exit 101
-      ;;
-  esac
-}
-
-process_dev_dependencies_build() {
-  echo "${MUSH_DEV_DEPS_BUILD}" | while IFS=$'\n' read dependency && [ -n "$dependency" ]; do
+  echo "${dependencies_build}" | while IFS=$'\n' read -r dependency && [ -n "$dependency" ]; do
     local package_name=${dependency%=*}
     local package_script=${dependency#*=}
     local package_dir="${MUSH_DEPS_DIR}/${package_name}"
--- tests/bare/features/simple-test.sh
+++ tests/bare/features/simple-test.sh
@@ -3,9 +3,9 @@ set -e
 
 echo "==> Build: mush"
 cp target/dist/mush target/dist/mush.sh
-bash target/dist/mush.sh -vv build --release
+bash target/dist/mush.sh build --release
 
 echo "==> Test: complex-app"
 cd tests/fixtures/complex-app
 rm -fr bin lib libexec share target
-bash ../../../target/dist/mush build
+bash ../../../target/dist/mush -vvvvvvvvv build
