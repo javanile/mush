8c30801a6228abbb1b20eee84b73275bbd0d3c1f
francescobianco <info.francescobianco@gmail.com>
2023-11-13 12:59:14 +0100

    First commit

--- tests/bare/build/cross-test.sh
+++ tests/bare/build/cross-test.sh
@@ -6,7 +6,11 @@ for from in debug release; do
     echo "Building from '${from}' to '${to}'..."
     echo "------------------------------------------------------------"
     cp "target/${from}/mush" "target/${from}/mush.sh"
-    bash "target/${from}/mush.sh" build --target "${to}"
+    if [ "${to}" = "release" ]; then
+        bash "target/${from}/mush.sh" build --release
+    else
+        bash "target/${from}/mush.sh" build
+    fi
     bash "target/${to}/mush" --version
     echo "(done)"
     echo ""
