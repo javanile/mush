8563bb15106d848622b2569b3a4211372ceec8e5
francesco <info.francescobianco@gmail.com>
2023-11-14 08:55:38 +0100

    First commit

--- tests/bare/build/auto-debug-debug-test.sh
+++ tests/bare/build/auto-debug-debug-test.sh
@@ -6,5 +6,5 @@ profile_to=debug
 
 ## Build Mush
 cp target/${profile_from}/mush target/${profile_from}/mush.sh
-MUSH_HOME=${PWD} bash target/${profile_from}/mush.sh build
+MUSH_HOME=${PWD}/.mush bash target/${profile_from}/mush.sh build
 bash target/${profile_to}/mush --version
