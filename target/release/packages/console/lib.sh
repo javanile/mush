1d586f1be9e7d23e401a8a632a88ad8c07fd2d4a
francescobianco <info.francescobianco@gmail.com>
2024-03-21 22:03:36 +0100

    Stable Release

--- target/release/packages/console/lib.sh
+++ target/release/packages/console/lib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## BP010: Release metadata
 ## @build_type: lib
-## @build_date: 2024-03-21T21:01:36Z
+## @build_date: 2024-03-21T21:03:23Z
 set -e
 use() { return 0; }
 extern() { return 0; }
