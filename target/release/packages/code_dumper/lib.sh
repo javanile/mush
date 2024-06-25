0133ce0f2b3d158f0fa06fddf8c5b9b9486996ee
francesco <info.francescobianco@gmail.com>
2024-06-25 17:16:01 +0200

    test

---
+++ target/release/packages/code_dumper/lib.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+## BP010: Release metadata
+## @build_type: lib
+## @build_date: 2023-11-14T09:34:33Z
+set -e
+extern() {
+  extern=$1
+}
+legacy() {
+  legacy=$1
+}
+module() {
+  module=$1
+}
+public() {
+  public=$1
+}
+use() {
+  use=$1
+}
+embed() {
+  embed=$1
+}
+## BP004: Compile the entrypoint
+
+code_dumper() {
+  local file
+  local line
+  local keyword
+  local column
+  local source
+  local padding
+  local keyword_padding
+  local message
+  local help
+
+  file=$1
+  line=$2
+  keyword=$3
+  message=$4
+  help=$5
+  source=$(sed -n "${line}p" "${file}")
+  column=${source%%$keyword*}
+  padding=$(echo "$line" | sed 's/[0-9]/ /g')
+  keyword_padding=$(echo "$column" | sed 's/./ /g')
+  keyword_size=$(echo "$keyword" | sed 's/./^/g')
+
+  echo -e "\e[0m${padding}\e[0m\e[0m\e[1m\e[38;5;12m--> \e[0m\e[0m${file}:${line}:${#column}\e[0m"
+  echo -e "\e[0m${padding} \e[0m\e[0m\e[1m\e[38;5;12m|\e[0m"
+  echo -e "\e[0m\e[1m\e[38;5;12m${line}\e[0m\e[0m \e[0m\e[0m\e[1m\e[38;5;12m|\e[0m\e[0m \e[0m\e[0m${source}\e[0m"
+  echo -e "\e[0m${padding} \e[0m\e[0m\e[1m\e[38;5;12m| ${keyword_padding}\e[0m\e[0m\e[1m\e[38;5;9m${keyword_size}\e[0m\e[0m \e[0m\e[0m\e[1m\e[38;5;9m${message}\e[0m"
+  if [ -n "${help}" ]; then
+    echo -e "\e[0m${padding} \e[0m\e[0m\e[1m\e[38;5;12m|\e[0m"
+    echo -e "\e[0m${padding} \e[0m\e[0m\e[1m\e[38;5;12m= \e[0m\e[1;39mhelp:\e[0m ${help}\e[0m"
+  fi
+  echo ""
+}
---
+++ target/release/packages/console/lib.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+## BP010: Release metadata
+## @build_type: lib
+## @build_date: 2023-11-14T09:34:35Z
+set -e
+extern() {
+  extern=$1
+}
+legacy() {
+  legacy=$1
+}
+module() {
+  module=$1
+}
+public() {
+  public=$1
+}
+use() {
+  use=$1
+}
+embed() {
+  embed=$1
+}
+## BP004: Compile the entrypoint
+
+# FATAL
+# ERROR
+# WARNING
+# INFO
+# DEBUG
+# TRACE
+# SUCCESS
+
+case "$(uname -s)" in
+  Darwin*)
+    ESCAPE='\x1B'
+    ;;
+  Linux|*)
+    ESCAPE='\e'
+    ;;
+esac
+
+#CONSOLE_INDENT="${ESCAPE}[1;33m{Mush}${ESCAPE}[0m"
+
+console_pad() {
+  [ "$#" -gt 1 ] && [ -n "$2" ] && printf "%$2.${2#-}s" "$1"
+}
+
+console_log() {
+  console_print "$1" "$2"
+}
+
+console_info() {
+  if [ "${VERBOSE}" -gt "0" ]; then
+    console_print "${ESCAPE}[1;36m$(console_pad "$1" 12)${ESCAPE}[0m" "$2"
+  fi
+}
+
+console_warning() {
+  console_print "${ESCAPE}[1;33m$(console_pad "$1" 12)${ESCAPE}[0m" "$2"
+}
+
+console_status() {
+  console_print "${ESCAPE}[1;32m$(console_pad "$1" 12)${ESCAPE}[0m" "$2"
+}
+
+console_error() {
+  echo -e "${ESCAPE}[1;31merror${ESCAPE}[0m: $1" >&2
+}
+
+console_error_code() {
+  echo -e "${ESCAPE}[1;31merror[$1]${ESCAPE}[1;39m: $2${ESCAPE}[0m" >&2
+}
+
+console_hint() {
+  echo -e "${ESCAPE}[1;39m$1${ESCAPE}[0m" >&2
+}
+
+console_print() {
+  if [ -z "${QUIET}" ]; then
+    echo -e "$1 $2" >&2
+  fi
+}
