91c324481715ba556a261ad1ebf1061391c231f5
francescobianco <info.francescobianco@gmail.com>
2024-12-19 20:07:12 +0100

    fix

---
+++ tests/bare/registry-test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+## Build Mush
+cp target/debug/mush target/debug/mush.sh
+bash target/debug/mush.sh build
+
+## Build Basic App
+cd tests/fixtures/basic-app
+rm -fr target lib && true
+bash ../../../target/debug/mush -vvv install demo
+bash ../../../target/debug/mush -vvv install dmeo
