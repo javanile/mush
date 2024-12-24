2e16b2a6692235f2416fa6b6bafd9db17baa55b5
francescobianco <info.francescobianco@gmail.com>
2024-12-24 21:45:30 +0100

    Development Commit

--- target/debug/packages/console/lib.sh
+++ target/debug/packages/console/lib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## BP010: Release metadata
 ## @build_type: lib
-## @build_date: 2024-12-24T10:39:46Z
+## @build_date: 2024-12-24T20:20:37Z
 set -e
 use() { return 0; }
 extern() { return 0; }
--- target/debug/packages/getoptions/lib.sh
+++ target/debug/packages/getoptions/lib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## BP010: Release metadata
 ## @build_type: lib
-## @build_date: 2024-12-24T10:39:49Z
+## @build_date: 2024-12-24T20:20:46Z
 set -e
 use() { return 0; }
 extern() { return 0; }
