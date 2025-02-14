b4242be1cdd179167cf3ddee3fbe9805be50230f
francesco <info.francescobianco@gmail.com>
2025-02-14 14:41:12 +0100

    Development Commit

--- target/debug/packages/code_dumper/lib.sh
+++ target/debug/packages/code_dumper/lib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ## BP010: Release metadata
 ## @build_type: lib
-## @build_date: 2025-01-28T18:37:11Z
+## @build_date: 2025-02-14T13:38:49Z
 set -e
 use() { return 0; }
 extern() { return 0; }
